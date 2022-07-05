29962133b246d8e5f16bca62cec3d0bd4b09a8af
yuedan.work <95392548+yuedanlabs@users.noreply.github.com>
2022-07-06 04:06:05 +0800

    Set default options when installed and open the options page

--- src/background.js
+++ src/background.js
@@ -2,10 +2,16 @@ const default_options = {
     api_url: "https://icp-query.ss-work.workers.dev/icp",
 };
 
-chrome.runtime.onInstalled.addListener(() => {
-    chrome.storage.sync.set(
-        {
-            options: default_options,
-        },
-    );
+chrome.runtime.onInstalled.addListener((res) => {
+    // https://developer.chrome.com/docs/extensions/reference/tabs/#opening-an-extension-page-in-a-new-tab
+    if (res.reason === chrome.runtime.OnInstalledReason.INSTALL) {
+        chrome.storage.sync.set(
+            {
+                options: default_options,
+            },
+        );
+        chrome.tabs.create({
+            url: "options/index.html",
+        });
+    }
 });
